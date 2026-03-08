85201f77f36664e9b37d693066b658bc3fda0d8a
qunientos65242-alt <qunientos65242@gmail.com>
2026-03-08 10:38:23 -0600

    Update ui_library.lua

--- ui_library.lua
+++ ui_library.lua
@@ -37,6 +37,13 @@ local Tabs = {
     Settings = Window:AddTab({ Title = "Settings",  Icon = "settings" }),
 }
 
+local Tabs = {
+    Main     = Window:AddTab({ Title = "Main",     Icon = "swords"   }), -- NUEVA
+    Profile  = Window:AddTab({ Title = "Profile",  Icon = "user"     }),
+    FullInfo = Window:AddTab({ Title = "Full Info", Icon = "monitor"  }),
+    Settings = Window:AddTab({ Title = "Settings",  Icon = "settings" }),
+}
+
 -- ── Initialize addons ─────────────────────────────────────────
 SaveManager:SetLibrary(Fluent)
 InterfaceManager:SetLibrary(Fluent)

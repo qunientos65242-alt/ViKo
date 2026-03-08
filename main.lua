e0dcfa246091577c163f4b1d812ba7db386f5223
qunientos65242-alt <qunientos65242@gmail.com>
2026-03-08 09:00:21 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,18 +1,12 @@
--- ViKo Hub | Lógica de Scripts
 local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/qunientos65242-alt/ViKo/main/ui_library.lua"))()
 
-local Window = Lib:CreateWindow("ViKo Hub | 2026")
+-- El nombre ahora se verá en mayúsculas y espaciado (Estilo Minimal)
+local Window = Lib:CreateWindow("ViKo Project")
 
-Window:AddButton("Velocidad x100", function()
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100
+Window:AddButton("Infinite Jump", function()
+    -- Tu código de salto aquí
 end)
 
-Window:AddButton("Salto Infinito", function()
-    game:GetService("UserInputService").JumpRequest:Connect(function()
-        game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):ChangeState("Jumping")
-    end)
-end)
-
-Window:AddButton("Reset Character", function()
-    game.Players.LocalPlayer.Character:BreakJoints()
+Window:AddButton("Speed Boost", function()
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 80
 end)
